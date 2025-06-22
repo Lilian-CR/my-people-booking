9a58066706bdf29a9726849a1cfeb3eff4320f6d
Lilian-CR <lilianxxx@gmail.com>
2025-06-22 07:51:40 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+// src/App.jsx
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
@@ -8,37 +9,37 @@ import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Concerts from "./pages/Concerts";
 import MyPeopleFest from "./pages/MyPeopleFest";
-import BandPage from "./pages/BandPage"; 
+import BandPage from "./pages/BandPage";
 
 function App() {
   return (
     <Router>
       <div className="flex flex-col min-h-screen bg-white">
-        {/* Navbar always on top */}
         <Navbar />
 
-        {/* Page content */}
         <main className="flex-grow h-[600px] overflow-y-auto">
-        <Routes>
-  <Route path="/" element={<Home />} />
-  <Route path="/roster" element={<Roster />} />
-  <Route path="/roster/:bandSlug" element={<BandPage />} />
-  <Route path="/concerts" element={<Concerts />} />
-  <Route path="/mypoplefest" element={<MyPeopleFest />} />
-  <Route path="/about" element={<About />} />
-  <Route path="/contact" element={<Contact />} />
-  <Route
-    path="*"
-    element={
-      <div className="text-center text-black p-20 text-2xl">
-        404 – Page Not Found
-      </div>
-    }
-  />
-</Routes>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/roster" element={<Roster />} />
+            <Route path="/concerts" element={<Concerts />} />
+            <Route path="/mypoplefest" element={<MyPeopleFest />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+
+            {/* Enables direct URLs for bands */}
+            <Route path="/:bandSlug" element={<BandPage />} />
+
+            <Route
+              path="*"
+              element={
+                <div className="text-center text-black p-20 text-2xl">
+                  404 – Page Not Found
+                </div>
+              }
+            />
+          </Routes>
         </main>
 
-        {/* Footer always at bottom */}
         <Footer />
       </div>
     </Router>
