e1cf9834af3aeee55f43661241153c1a8d90350c
Lilian Rodrigues <lilianxxx@gmail.com>
2026-01-29 02:29:15 +0100

    Refactor band bio in roster.js for clarity
    Removed unnecessary HTML tags and improved formatting in the band bio.

--- src/data/roster.js
+++ src/data/roster.js
@@ -7,28 +7,14 @@ const roster = [
     slug: "nothing-works",
     image: "/images/nothing-works.jpg",
     bio: `
- <p>
-    Formed in late 2022, <strong>Nothing Works</strong> is a rising force from Berlin’s underground subcultures. Blending '90s indie/alternative with modern melodic and post-hardcore, their sound is raw, urgent and emotionally charged. Think Drug Church meets Bikini Kill. That’s a good starting point for understanding their unapologetically political lyrics and the fierce energy they bring to every show.
-  </p>
+Formed in late 2022, <strong>Nothing Works</strong> is a rising force from Berlin’s underground subcultures. Blending '90s indie/alternative with modern melodic and post-hardcore, their sound is raw, urgent and emotionally charged. Think Drug Church meets Bikini Kill. That’s a good starting point for understanding their unapologetically political lyrics and the fierce energy they bring to every show.
+Their debut EP, <i>"Bold Talk for a Burner Account"</i>, released in July 2025, features four songs that give a clear sense of what they’re about. Embrace for impact!
+The band, composed entirely of FLINTA* members, is quickly becoming a standout in the local scene, known for both their message and their momentum. Despite being young, Nothing Works has already supported notable acts from Austria, Canada, Germany, Mexico, United States & Peru — including CURB, PUNITIVE DAMAGE, YEAHRS, SHORELINE, GIRLS GO SKA, GO! NYHC, DRY SOCKET & TOMAR CONTROL.
+Their mission? To make people move — dance, stage dive — while also raising awareness about political struggles and everyday resistance.
 
-  <p>
-    Their debut EP, <i>"Bold Talk for a Burner Account"</i>, released in July 2025, features four songs that give a clear sense of what they’re about. Embrace for impact!
-  </p>
+FFO:</b> Gouge Away, Drug Church, Scowl</p>
 
-  <p>
-    The band, composed entirely of FLINTA* members, is quickly becoming a standout in the local scene, known for both their message and their momentum. Despite being young, Nothing Works has already supported notable acts from Austria, Canada, Germany, Mexico, United States & Peru — including CURB, PUNITIVE DAMAGE, YEAHRS, SHORELINE, GIRLS GO SKA, GO! NYHC, DRY SOCKET & TOMAR CONTROL.
-  </p>
-
-  <p>
-    Their mission? To make people move — dance, stage dive — while also raising awareness about political struggles and everyday resistance.
-  </p>
-
-  <p><b>FFO:</b> Gouge Away, Drug Church, Scowl</p>
-
-  <p>
-    <b>Photo Credit for promo pic:</b><br />
-    Oyèmi Noize
-  </p>
+Photo Credit for promo pic: Oyèmi Noize
 `,
     videos: [
       "https://www.youtube.com/embed/SL1Qd1AI5eE?si=zISQnByoij8yidCm",
