e5c205043d0d07777dba5cec763375fb31650bd1
Lilian Rodrigues <lilianxxx@gmail.com>
2026-01-29 05:39:25 +0100

    Update roster.js

--- src/data/roster.js
+++ src/data/roster.js
@@ -57,129 +57,188 @@ socials: {
   },
 
   {
-    id: 3,
-    name: "TOMAR CONTROL",
-    slug: "tomar-control",
-    image: "/images/tomar-control.png",
-    video: "https://www.youtube.com/embed/E05C3fK68jo?si=R5dyqDd0lpjVWylt",
-    pressKit:
-      "https://www.dropbox.com/scl/fo/bdxyb8mj4086wphoayfqi/ADlufLIzBnFUkqwGSd1UzHw?rlkey=3v83fzmcshkz1q0diql186z6c&e=1&st=1dfs9k4d&",
-    bandsintownId: "11339622",
-    socials: {
-      instagram: "https://instagram.com/tomarcontrol",
-      facebook: "https://facebook.com/xtomarcontrolx",
-      spotify:
-        "https://open.spotify.com/artist/5Lqk0KuGJcKmnHUu6Fvk5C?si=yYJzNdqeTU-Lu9DY0DIVlw",
-      bandcamp: "https://tomarcontrol.bandcamp.com",
-    },
+  id: 3,
+  name: "TOMAR CONTROL",
+  slug: "tomar-control",
+  image: "/images/tomar-control.png",
+  bio: `
+Coming Soon!
+`,
+  videos: [
+    "https://www.youtube.com/embed/yHiRiy7EJfY?si=TtGz3xXVeSInVy7p",
+    "https://www.youtube.com/embed/I-RFARA2YhM?si=ZjGPbpaEiGSpX6F2",
+    "https://www.youtube.com/embed/QIoHka5u5C0?si=BebTnwH8TCMst0tP",
+  ],
+  socials: {
+    instagram: "https://instagram.com/tomarcontrol",
+    facebook: "https://facebook.com/xtomarcontrolx",
+    bandcamp: "https://tomarcontrol.bandcamp.com",
   },
+  pressKit:
+    "https://www.dropbox.com/scl/fo/bdxyb8mj4086wphoayfqi/ADlufLIzBnFUkqwGSd1UzHw?rlkey=3v83fzmcshkz1q0diql186z6c&e=1&st=1dfs9k4d&",
+  bandsintownId: "11339622",
+},
 
-  {
-    id: 4,
-    name: "ALIEANATION",
-    slug: "alieanation",
-    image: "/images/alieanation.jpg",
-    video: "https://www.youtube.com/embed/x1GZUcIf0CI?si=uAP9y-izk5CYyaQb",
-    pressKit:
-      "https://www.dropbox.com/scl/fo/1s28nai1nvpye6dtrmm2b/ACb4DTg5E1UtmEDt5ZguZ0k?rlkey=dlm7i5yjv3zzx8cz467lt04hw&e=1&st=0x1zpvoo&",
-    socials: {
-      instagram: "https://www.instagram.com/alieanationband",
-      facebook: "https://www.facebook.com/alieanationband/",
-      spotify: "https://open.spotify.com/intl-de/artist/2XfCXoMKw2P8fx0dpaBDGl",
-      songkick: "https://www.songkick.com/artists/10230363-alieanation",
-    },
-    songkickId: "10230363",
+{
+  id: 4,
+  name: "ALIEANATION",
+  slug: "alieanation",
+  image: "/images/alieanation.jpg",
+  bio: `
+Coming Soon!
+`,
+  videos: [
+    "https://www.youtube.com/embed/x1GZUcIf0CI?si=el3avUvtcW3VTcP4",
+    "https://www.youtube.com/embed/gWNc7lKiVOE?si=H0MpYOeLG5QLBavQ",
+    "https://www.youtube.com/embed/dLjQoL9nCiw?si=7jKZiviD_I6a-dJK",
+  ],
+  socials: {
+    instagram: "https://www.instagram.com/alieanationband",
+    facebook: "https://www.facebook.com/alieanationband/",
+    songkick: "https://www.songkick.com/artists/10230363-alieanation",
   },
+  pressKit:
+    "https://www.dropbox.com/scl/fo/1s28nai1nvpye6dtrmm2b/ACb4DTg5E1UtmEDt5ZguZ0k?rlkey=dlm7i5yjv3zzx8cz467lt04hw&e=1&st=0x1zpvoo&",
+  songkickId: "10230363",
+},
 
-  {
-    id: 5,
-    name: "ÁSPERØ",
-    slug: "aspero",
-    image: "/images/AASPERØ.jpg",
-    video: "https://www.youtube.com/embed/pgO7IRQOd34",
-    pressKit:
-      "https://www.dropbox.com/scl/fo/zd18l8moivz3z58jdzsa8/AEsKy9A4J8FdX2DSzPIMYUQ?rlkey=9rk5q6xistz74mli149hcme8p&e=1&st=hyb6rlap&",
-    socials: {
-      instagram: "https://www.instagram.com/oaspero",
-      facebook: "https://www.facebook.com/oaspero/",
-      spotify:
-        "https://open.spotify.com/artist/2EuHNZcjTafuukTqpx9VEe?si=JLMqXrWiQ1CLk6v1yTM_RQ",
-      bandcamp: "https://oaspero.bandcamp.com/",
-      songkick: "https://www.songkick.com/artists/10252910",
-    },
-    songkickId: "10252910",
+{
+  id: 5,
+  name: "ÁSPERØ",
+  slug: "aspero",
+  image: "/images/AASPERØ.jpg",
+  bio: `
+Coming Soon!
+`,
+  videos: [
+    "https://www.youtube.com/embed/pgO7IRQOd34?si=1HHXbJiIYnHAj2K-",
+    "https://www.youtube.com/embed/tE2oLkPI8jk?si=hmYm-gCrJccamVnc",
+    "https://www.youtube.com/embed/AB9tGbX2eiQ?si=6X591SNmuvVpb2fB",
+  ],
+  socials: {
+    instagram: "https://www.instagram.com/oaspero",
+    facebook: "https://www.facebook.com/oaspero/",
+    bandcamp: "https://oaspero.bandcamp.com/",
   },
+  pressKit:
+    "https://www.dropbox.com/scl/fo/zd18l8moivz3z58jdzsa8/AEsKy9A4J8FdX2DSzPIMYUQ?rlkey=9rk5q6xistz74mli149hcme8p&e=1&st=hyb6rlap&",
+  songkickId: "10252910",
+},
 
-  {
-    id: 6,
-    name: "DERRUMBANDO DEFENSAS",
-    slug: "derrumbando-defensas",
-    image: "/images/derrumbando-defensas.jpg",
-    video: "https://www.youtube.com/embed/tQSldldcu1I?si=wusY_72hujAalpCC",
-    pressKit: "https://www.dropbox.com/scl/fo/0b051j5uv92rzxq7zcfk8/h?rlkey=ahtavaokbmrtrx6urmcwyswxf&",
-    socials: {
-      instagram: "https://www.instagram.com/derrumbandodefensas",
-      facebook: "https://www.facebook.com/derrumbando.defensas",
-      spotify: "https://open.spotify.com/artist/4amZV5nKqgW1mE8ZsLCiCX",
-      bandcamp: "https://negroleo.bandcamp.com",
-      songkick: "https://www.songkick.com/artists/9150954-derrumbando-defensas",
-    },
-    songkickId: "9150954",
+{
+  id: 6,
+  name: "DERRUMBANDO DEFENSAS",
+  slug: "derrumbando-defensas",
+  image: "/images/derrumbando-defensas.jpg",
+  bio: `
+Coming Soon!
+`,
+  videos: [
+    "https://www.youtube.com/embed/tQSldldcu1I?si=310zRG14QDNvjQq8",
+    "https://www.youtube.com/embed/MsmNhPMF1Nw?si=ttTaGB4oE3QR-hzt",
+    "https://www.youtube.com/embed/9VjiH_IuGRg?si=ooaVcYldT6wQQNTQ",
+  ],
+  socials: {
+    instagram: "https://www.instagram.com/derrumbandodefensas",
+    facebook: "https://www.facebook.com/derrumbando.defensas",
+    bandcamp: "https://negroleo.bandcamp.com",
   },
+  pressKit:
+    "https://www.dropbox.com/scl/fo/0b051j5uv92rzxq7zcfk8/h?rlkey=ahtavaokbmrtrx6urmcwyswxf&",
+  songkickId: "9150954",
+},
 
-  {
-    id: 7,
-    name: "FUTURO",
-    slug: "futuro",
-    image: "/images/futuro.jpg",
-    video: "https://www.youtube.com/embed/7OTyQy3xLWE?si=k5DlgLNIax2782vf",
-    pressKit:
-      "https://www.dropbox.com/scl/fo/5uy3mnros4qsz536cbknr/AKjbDTJLix0sjAGGr6fcc_Y?rlkey=ycva0ndd0ghn0c7lx75dnyi33&st=xy92w4pr&",
-    socials: {
-      instagram: "https://www.instagram.com/futuro_is_a_band",
-      facebook: "https://www.facebook.com/futurotheband/",
-      spotify:
-        "https://open.spotify.com/intl-pt/artist/1d2XZWUFD8AbtzC3E1h3b0?si=6FO80nJOT5OAyjpsdiQSxw",
-      bandcamp: "https://futuro.bandcamp.com/",
-      songkick: "https://www.songkick.com/artists/8379823",
-    },
-    songkickId: "8379823",
+{
+  id: 7,
+  name: "FUTURO",
+  slug: "futuro",
+  image: "/images/futuro.jpg",
+  bio: `
+Coming Soon!
+`,
+  videos: [
+    "https://www.youtube.com/embed/7OTyQy3xLWE?si=ANtAcm87Qn4DFd9O",
+    "https://www.youtube.com/embed/HJQ8WYwZugY?si=ogpiQoqJFxT_ZT5I",
+    "https://www.youtube.com/embed/pxtW8eXlYUU?si=LLJxDy9GODYVPlnU",
+  ],
+  socials: {
+    instagram: "https://www.instagram.com/futuro_is_a_band",
+    facebook: "https://www.facebook.com/futurotheband/",
+    bandcamp: "https://futuro.bandcamp.com/",
   },
+  pressKit:
+    "https://www.dropbox.com/scl/fo/5uy3mnros4qsz536cbknr/AKjbDTJLix0sjAGGr6fcc_Y?rlkey=ycva0ndd0ghn0c7lx75dnyi33&st=xy92w4pr&",
+  songkickId: "8379823",
+},
 
-  {
-    id: 8,
-    name: "PACIFIST",
-    slug: "pacifist",
-    image: "/images/pacifist.jpg",
-    video: "https://www.youtube.com/embed/1todhq1qp_E?si=T1aweNxj9iHyXqWN",
-    pressKit:
-      "https://www.dropbox.com/scl/fo/4jasph9s0s8tsvdeaxg4k/AJh4DrfK6YeHIMhilGNtzSA?rlkey=30kn9kxl76vm5odz19a1kn4ip&st=zc5tuoza&",
-    socials: {
-      instagram: "https://www.instagram.com/pacifist.india",
-      facebook: "https://www.facebook.com/PacifistIndia",
-      spotify:
-        "https://open.spotify.com/intl-pt/artist/0squZVgmZcAXfDPDeFtlPI?si=NY78NaFATIKopdOMdvsshA",
-      bandcamp: "https://pacifistindia.bandcamp.com/",
-      songkick: "https://www.songkick.com/artists/10149615",
-    },
-    songkickId: "10149615",
+{
+  id: 8,
+  name: "PACIFIST",
+  slug: "pacifist",
+  image: "/images/pacifist.jpg",
+  bio: `
+Coming Soon!
+`,
+  videos: [
+    "https://www.youtube.com/embed/0Pa4qIE_m1g?si=TSL-pmtUm7lGTYDZ",
+    "https://www.youtube.com/embed/6oNZG_n1eRs?si=IP0J9fD4i9Q6ASkq",
+    "https://www.youtube.com/embed/b9M4tEnZF5w?si=R7N2Gd2ZWHj4eCSn",
+  ],
+  socials: {
+    instagram: "https://www.instagram.com/pacifist.india",
+    facebook: "https://www.facebook.com/PacifistIndia",
+    bandcamp: "https://pacifistindia.bandcamp.com/",
   },
+  pressKit:
+    "https://www.dropbox.com/scl/fo/4jasph9s0s8tsvdeaxg4k/AJh4DrfK6YeHIMhilGNtzSA?rlkey=30kn9kxl76vm5odz19a1kn4ip&st=zc5tuoza&",
+  songkickId: "10149615",
+},
 
+{
+  id: 9,
+  name: "SWISS PLEASURE",
+  slug: "swiss-pleasure",
+  image: "/images/swiss-pleasure.jpg",
+  bio: `
+Coming Soon!
+`,
+  videos: [
+    "https://www.youtube.com/embed/YacSGr9W-wQ?si=oWK8ZFviyvgtqxTn",
+    "https://www.youtube.com/embed/yRYGoFsJltA?si=6uS7matJk0zG7VRi",
+    "https://www.youtube.com/embed/M6fnhckBh58?si=0Cn2-cCX8pmJlLzQ",
+  ],
+  socials: {
+    instagram: "https://www.instagram.com/swiss.pleasure/",
+    facebook: "https://www.facebook.com/swssplsr/",
+    bandcamp: "https://swisspleasure.bandcamp.com/",
+  },
+  pressKit:
+    "https://www.dropbox.com/scl/fo/y01ava5u5hmyaaybj9mdz/h?rlkey=c2ol7dgt7365jcdbezch00w0j&",
+  bandsintownId: "15521103",
+},
   {
-    id: 9,
-    name: "SWISS PLEASURE",
-    slug: "swiss-pleasure",
-    image: "/images/swiss-pleasure.jpg",
-    video: "https://www.youtube.com/embed/YacSGr9W-wQ?si=zcVGXxwhG6C9g38t",
-    pressKit: "https://www.dropbox.com/scl/fo/y01ava5u5hmyaaybj9mdz/h?rlkey=c2ol7dgt7365jcdbezch00w0j&",
-    bandsintownId: "15521103",
-    socials: {
-      instagram: "https://www.instagram.com/swiss.pleasure/",
-      facebook: "https://www.facebook.com/swssplsr/",
-      spotify:
-        "https://open.spotify.com/intl-pt/artist/6fQuTJUCDwLbzQvJV9xEgB?si=JLEGIjoNQG6e9UoXZlz6nw",
-      bandcamp: "https://swisspleasure.bandcamp.com/",
-    },
+  id: 10,
+  name: "URSULA",
+  slug: "ursula",
+  image: "/images/ursula.jpg",
+  bio: `
+Ursula blurs the line of every conventional facet of underground noise. Metal, punk, hardcore, grind, crust, thrash, they all apply yet none define them. Hailing from Apple Valley, CA, Ursula has been active as a touring band since 2017. Their debut full-length, I Don’t Like Anything (2025), was released on legendary southern California label Indecision Records (Unbroken, Throwdown, Bleeding Through, Ensign, Death by Stereo, etc.). Recorded, produced, and mastered by Paul Miner (Death by Stereo/Thrice/Atreyu) at Buzzbomb Studios, Orange, CA. Ursula previously released 3 EPs on the Indecision Records label: Meet Is Murder (2017, co-released with Programme Sounds), Regurgitate (2019), and Fickle (2020). Ursula has performed tour dates across California, Pacific Northwest, Midwest, Southwest, and Mexico. Ursula has opened for bands such as Terror, Agnostic Front, Unbroken, Bleeding Through, Cro Mags, Strife, 411, and many other legendary acts in the punk and hardcore community. “Often described as a hardcore band, Ursula swims in a chaotic ocean of influences reminiscent of 1990’s screamo, 2000’s sasscore, and the general vibe of Three One G bands. Make no mistake, the band’s DIY approach, presence in Orange County’s Programme scene, and interpersonal struggle and vegan lyrical themes keep them firmly rooted in the world of hardcore” – Visions Magazine and Evil Greed
+ “i don’t like anything” Ursula's new LP on Indecision Records, released April 2025!
+`,
+  videos: [
+    "https://www.youtube.com/embed/R22XteGbD04?si=d7oz6qtKdcMI2EsX",
+    "https://www.youtube.com/embed/Tu_hVcnVtGY?si=6AQ458BvYuXCTo7v",
+    "https://www.youtube.com/embed/fbbErwLIkn4?si=xHu0KQ3C_z-HJOVO",
+  ],
+  socials: {
+    instagram: "https://www.instagram.com/weareursulaband",
+    facebook: "https://www.facebook.com/weareursulaband",
+    bandcamp: "https://ursula.bandcamp.com",
+  },
+  pressKit:
+    "https://www.dropbox.com/scl/fo/yah9m6qwq4rb2uvq70m36/AOz1jAzNZK6eY9HmmLbDvF8?rlkey=oegkbdlm3ito8djanlged8kzm&st=p641rrzg&dl=0",
+  songkickId: "10107713",
+},
   },
 ];
 
